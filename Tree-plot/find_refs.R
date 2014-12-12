0c9ae77307fa7ecc67585bd9cf7509ffc6a41ae9
Derek Young <derek.j.n.young@gmail.com>
2014-12-11 17:25:24 -0800

    Runs iterations to seek refs sequentially down chain, but may have bug as thinks that in the second iteration all refs are other trees

--- Tree-plot/find_refs.R
+++ Tree-plot/find_refs.R
@@ -2,27 +2,37 @@ setwd("C:/Users/DYoung/Dropbox/Research projects/PSME climate adaptation/Data-an
 
 # open data file
 trees <- read.csv("Field data/2014 field data/adult_trees_merged.csv")
-refs <- read.csv("GPS data/2014 refs/refs 2014.csv")
+refs <- read.csv("GPS data/2014 refs/refs 2014 merged.csv")
 ref.ids <- toupper(refs$name)
 
 #select only focal trees for ref extraction
 trees.foc <- trees[toupper(trees$foc) == "X",]
 tree.ids <- toupper(trees.foc$tree.id)
 
+### RUN A LOOP TO FIND ALL BASE LOCATION REFERENCE FOR ALL TREES, EVEN ONES THAT DEPEND ON A CHAIN OF REFERENCES
 
 i <- 1 # keep track of current iteration
+unresolved.df <- NULL # store the ref resolution for the trees in each iteration
 
 # for each tree that needs its reference resolved, find its id and the listed ref
 unresolved.tree.id <- as.character(trees.foc$tree.id)
 unresolved.tree.ref <- as.character(trees.foc$ref)
 
-#check if ref is another tree, is in refs file or neither
-ref.is.self <- (unresolved.tree.ref == unresolved.tree.id)
-ref.is.tree <- (unresolved.tree.ref %in% tree.ids) & (!ref.is.self)
-ref.is.ref <- (unresolved.tree.ref %in% ref.ids)
-ref.missing <- !(ref.is.tree | ref.is.ref)
 
-unresolved.df <- data.frame(unresolved.tree.id,unresolved.tree.ref,ref.is.self,ref.is.ref,ref.is.tree,ref.missing)
+for(i in 1:3) {
 
-unresolved.df[unresolved.df$ref.missing==TRUE,]
-
+  #check if ref is self, another tree, is in refs file, or missing, and merge results into dataframe
+  ref.is.self <- (unresolved.tree.ref == unresolved.tree.id)
+  ref.is.tree <- (unresolved.tree.ref %in% tree.ids) & (!ref.is.self)
+  ref.is.ref <- (unresolved.tree.ref %in% ref.ids)
+  ref.missing <- !(ref.is.tree | ref.is.ref)
+  unresolved.df[[i]] <- data.frame(unresolved.tree.id,unresolved.tree.ref,ref.is.self,ref.is.ref,ref.is.tree,ref.missing)
+  
+  #make a list of the trees missing refs
+  missing.list <- unresolved.df[[i]][unresolved.df[[i]]$ref.missing==TRUE,]
+  
+  #prepare list of the next unresolved trees (trees for which ref is another tree)
+  unresolved.tree.id <- as.character(unresolved.df[[i]][unresolved.df[[1]]$ref.is.tree==TRUE,]$unresolved.tree.id)
+  unresolved.tree.ref <- as.character(unresolved.df[[i]][unresolved.df[[1]]$ref.is.tree==TRUE,]$unresolved.tree.ref)
+  
+}

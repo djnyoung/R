3717df8213af859966e1d621d0b899957d155f52
Derek Young <derek.j.n.young@gmail.com>
2014-12-12 08:47:26 -0800

    Pasted in old code for location calculation--need to update to run with new variables and data structure

--- Tree-plot/find_refs.R
+++ Tree-plot/find_refs.R
@@ -42,13 +42,14 @@ while(finished == FALSE) {
 }
 
 #get all refs out in order
-sorted.refs <- do.call("rbind",rev(unresolved.df))
-sorted.refs.unique <- unique(sorted.refs) # saves the first occurrence of each tree, so now they are sorted such that earlier ones depend on later ones
-
+sorted.trees <- do.call("rbind",rev(unresolved.df))
+sorted.trees.unique <- unique(sorted.trees) # saves the first occurrence of each tree, so now they are sorted such that earlier ones depend on later ones
+trees.loclookup <- merge(sorted.trees.unique,trees.foc, by.x="unresolved.tree.id",by.y="tree.id")
+names(trees.loclookup)[1:2] <- c("tree.id","tree.ref")
 
 ##### COMPUTE LOCATION INFORMATION #####
 
-### first define functions ###
+### first define convenience functions ###
 
 # function for getting a given ref's location
 refloc <- function(ref.name) {
@@ -76,10 +77,65 @@ offsety <- function(y,bearing,distance,back.brg) {   #coordinates of reference, 
   y + cos(bearing*pi/180)*distance
 }
 
-### end define functions ###
+### end define convenience functions ###
+
+### begin script for looping through trees one-by-one and looking up their location information (either from refs or as offsed from previous tree)
+
+#compute true bearing based on magnetic north bearing and declination
+trees$bearing.true <- (trees$bearing + 13.5) %% 360
+refs$bearing.true <- (refs$bearing + 13.5) %% 360
+
+# fill in coordinates for refs that reference other refs
+unk_refs <- refs[which(is.na(refs$x)),]
+unk_refs$x <- offsetx(refloc(unk_refs$ref)$x,unk_refs$bearing.true,unk_refs$dist)
+unk_refs$y <- offsety(refloc(unk_refs$ref)$y,unk_refs$bearing.true,unk_refs$dist)
+refs[which(is.na(refs$x)),] <- unk_refs
+
+
+## output all refs
+write.csv(refs,"refs.csv")
 
 
 
+# fill in coordinates for trees
+# must do this in a for loop because some later trees reference earlier trees
 
+trees$x <- rep(NA,nrow(trees))
+trees$y <- rep(NA,nrow(trees))
+
+for(i in 1:nrow(trees)) {
+  
+  if(trees$ref.type[i] == 'ref') {
+    
+    if (nrow(refloc(trees$ref[i]))==0) {
+      print("Reference requested not found: ")
+      print(trees$ref[i])
+    } else if(is.na(refloc(trees$ref[i])$x)) {
+      print("Reference requested does not have coordinates: ")
+      print(trees$ref[i])
+    } else if(is.na(trees$bearing.true[i])) { # there are no coordinates, so set the tree equal to the ref coords
+      trees[i,]$x <- refloc(trees$ref[i])$x
+      trees[i,]$y <- refloc(trees$ref[i])$y
+    } else {      
+      trees[i,]$x <- offsetx(refloc(trees$ref[i])$x,trees$bearing.true[i],trees$dist[i])
+      trees[i,]$y <- offsety(refloc(trees$ref[i])$y,trees$bearing.true[i],trees$dist[i])
+    }
+  } else { #the reference is another tree
+    if(nrow(treeloc(trees$ref[i]))==0) {
+      print("Tree requested as reference not found: ")
+      print(trees$ref[i])
+    } else if (is.na(treeloc(trees$ref[i])$x)) {
+      print("Tree requested as reference does not have coordinates: ")
+      print(trees$ref[i])
+    } else {
+      trees[i,]$x <- offsetx(treeloc(trees$ref[i])$x,trees$bearing.true[i],trees$dist[i])
+      trees[i,]$y <- offsety(treeloc(trees$ref[i])$y,trees$bearing.true[i],trees$dist[i])
+    }
+    
+  }
+  
+}
+
+write.csv(trees,"trees_loc.csv")
 
 
